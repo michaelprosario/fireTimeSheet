e75a5d7ced77fac07f06274a52aaaac6c4bbe307
michaelprosario <michael.p.rosario@gmail.com>
2018-11-20 10:29:48 +0000

    Edit layout of time entry screen and validation

--- components/create-time-entry.js
+++ components/create-time-entry.js
@@ -1,19 +1,4 @@
 
-/*
-x - default date to today
-x- enter start time
-x- enter end time
-x- calculate hours
-x- make sure project is required
-x- make sure story is required
-x- provide a way to add notes
-
-- implement save operation for form data
-- how do we pass in the time sheet id?
-- use a two col format for the screen
-
-*/
-
 class CreateTimeEntryComponent {
     constructor(objDataServices) {
         this.fsDataServices = objDataServices;
@@ -51,7 +36,6 @@ data: function () {
     dateToday = getShortDateFormat(dateToday);
 
     return {
-        
         startTime: '',
         endTime: '',
         hours: '',
@@ -59,7 +43,9 @@ data: function () {
         story: '',
         task: '',
         date: dateToday,
-        notes: ''
+        notes: '',
+        errors: [],
+        timeSheetId: currentTimeSheetId
     }
 },
 
@@ -81,35 +67,35 @@ methods:
     },
 
     getFormErrors: function() {
-        var errors = [];
+        this.errors = [];
         console.log(this);
 
         if(this.project ===""){
-            errors.push("Project is required.")
+            this.errors.push("Project is required.")
         }
         
         if(this.story ===""){
-            errors.push("Story is required.")
+            this.errors.push("Story is required.")
         }     
         
         if(!isValidTime(this.startTime)) {
-            errors.push('Valid start time is required');
+            this.errors.push('Valid start time is required');
         }
         
         if(!isValidTime(this.endTime)) {
-            errors.push('Valid end time is required');
+            this.errors.push('Valid end time is required');
         }
         
         if(isNaN(Date.parse(this.date))) {
-            errors.push("Please provide a valid date")
+            this.errors.push("Please provide a valid date")
         }
         
-        // date should be valid date
-        
         // hours should be a float
+        if(isNaN(parseFloat(this.hours))){
+            this.errors.push("Enter a valid number for the hours");
+        }
 
-
-        return errors;
+        return this.errors;
     },
 
     handleCreateTimeEntry: function(){
@@ -129,103 +115,135 @@ methods:
 template: `
 <div>
 
+
+
+<div class="container">
 <h1>Create Time Entry</h1>
-<div class="control-group">
-    <label for="date" class="control-label">
-        Date
-    </label>
-    <div class="controls">
-        <input  name="txtDate" 
-                type="text"  
-                id="txtDate"
-                v-model:value="date"
-                >
-    </div>
+
+<div>
+    <ul v-for="error in errors">
+        <li style="color:red">{{error}}</li>
+    </ul>
 </div>
-<div class="control-group">
-    <label for="start" class="control-label">
+
+</div>
+<div class="container">
+
+<div class="row">
+    <div class="col">
+        <label for="start" class="control-label">
         Start
-    </label>
-    <div class="controls">
-        <input  name="txtStart" 
-                type="text" 
-                v-model:value="startTime" 
-                id="txtStart"
-                v-on:blur="handleOnBlurTime()"
-                >
+        </label>
+        <div class="controls">
+            <input  name="txtStart" 
+            type="text" 
+            v-model:value="startTime" 
+            id="txtStart"
+            v-on:blur="handleOnBlurTime()"
+            >
+        </div>
     </div>
-</div>
-<div class="control-group">
-    <label for="start" class="control-label">
+    <div class="col">
+        <label for="start" class="control-label">
         End
-    </label>
-    <div class="controls">
-        <input  name="txtEnd" 
-                type="text" 
-                v-model:value="endTime" 
-                id="txtEnd" 
-                v-on:blur="handleOnBlurTime()">
+        </label>
+        <div class="controls">
+            <input  name="txtEnd" 
+            type="text" 
+            v-model:value="endTime" 
+            id="txtEnd" 
+            v-on:blur="handleOnBlurTime()">
+        </div>    
     </div>
-</div>
-<div class="control-group">
-    <label for="hours" class="control-label">
-        Hours
-    </label>
-    <div class="controls">
-        <input  name="txtHours" 
-                type="text" 
-                id="txtHours" 
-                v-model:value="hours">
+</div> <!-- end row -->
+
+        
+<div class="row">
+    <div class="col">
+        <label for="hours" class="control-label">
+            Hours
+        </label>
+        <div class="controls">
+            <input  name="txtHours" 
+                    type="text" 
+                    id="txtHours" 
+                    v-model:value="hours">
+        </div>                
     </div>
-</div>
-<div class="control-group">
-    <label for="story" class="control-label">
-        Project
-    </label>
-    <div class="controls">
-        <input  name="txtProject" 
-                type="text" 
-                id="txtProject"
-                v-model:value="project"
-                >
-    </div>
-</div>
-<div class="control-group">
-    <label for="story" class="control-label">
-        Story
-    </label>
-    <div class="controls">
-        <input  name="txtStory" 
-                type="text" 
-                id="txtStory"
-                v-model:value="story"
-                >
-    </div>
-</div>
-<div class="control-group">
-    <label for="task" class="control-label">
-        Task
-    </label>
-    <div class="controls">
-        <input  name="txtTask" 
-                type="text"id="txtTask"
-                v-model:value="task"
-                >
-    </div>
-</div>
-<div class="control-group">
-    <label for="task" class="control-label">
-        Notes
-    </label>
-    <div class="controls">
-        <textarea   name="txtNotes" 
-                    type="text"
-                    id="txtNotes"
-                    v-model:value="notes"
+    <div class="col">
+        <label for="date" class="control-label">
+            Date
+        </label>
+        <div class="controls">
+            <input  name="txtDate" 
+                    type="text"  
+                    id="txtDate"
+                    v-model:value="date"
                     >
-        </textarea>
+        </div>
     </div>
+</div><!-- end row -->
+
+
+<div class="row">
+    <div class="col">
+        <label for="story" class="control-label">
+            Project
+        </label>
+        <div class="controls">
+            <input  name="txtProject" 
+                    type="text" 
+                    id="txtProject"
+                    v-model:value="project"
+                    >
+        </div>
+    </div>
+    <div class="col">
+        <label for="story" class="control-label">
+            Story
+        </label>
+        <div class="controls">
+            <input  name="txtStory" 
+                    type="text" 
+                    id="txtStory"
+                    v-model:value="story"
+                    >
+        </div>
+    </div>
+</div> <!-- end row -->
+
+
+<div class="row">
+    <div class="col">
+        <label for="task" class="control-label">
+            Task
+        </label>
+        <div class="controls">
+            <input  name="txtTask" 
+                    type="text"id="txtTask"
+                    v-model:value="task"
+                    >
+        </div>
+    </div>
+    <div class="col">
+        <label for="task" class="control-label">
+            Notes
+        </label>
+        <div class="controls">
+            <textarea   name="txtNotes" 
+                        type="text"
+                        id="txtNotes"
+                        v-model:value="notes"
+                        >
+            </textarea>
+        </div>
+    </div>
+</div><!-- end row -->
+            
 </div>
+
+
+
 
 <br>
 <div class="form-actions">
@@ -237,6 +255,7 @@ template: `
         Save
     </button>
 </div>
+
 
 </div>
 `
